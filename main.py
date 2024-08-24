46580c310693211a813bf04e5537a92ec6be3d7a
SouthernCure <mekhigh10@gmail.com>
2024-08-24 17:47:58 -0400

    Fixed Syntax error

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ time.sleep(1)
 
 # Determines Timezone
 customTimezone = input("Would you like to use a custom timezone? If not, it will be set to your systems time zone (reccommended) (y/n)")
-if customTimezone == n:
+if customTimezone == "n":
     timezone = open("/etc/timezone", "r").readline()
 else:
     print ("Please refer to this guide: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones")
